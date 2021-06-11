b588def822203ccf7d269a9ecf5bb66fbdec31fe
0mdx1 <0x0mickendel@gmail.com>
2021-06-12 00:08:19 +0300

    Added possibility to get user email via token

---
+++ marketplace-client/src/marketplace/src/app/_services/checkout/checkout.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { CartItem } from "src/app/_models/cart-item.model";
+import { User } from "src/app/_models/user";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Checkout {
+
+  sendOrderDetails(user: User, cart: CartItem[]) {
+    
+    
+    
+    
+  }
+}
--- marketplace-client/src/marketplace/src/app/checkout/checkout.component.ts
+++ marketplace-client/src/marketplace/src/app/checkout/checkout.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "../_auth/auth.service";
 import { CartItem } from "../_models/cart-item.model";
 import { CartService } from "../_services/cart/cart.service";
+import { Checkout } from "../_services/checkout/checkout.service";
 
 @Component({
   selector: 'mg-checkout',
@@ -8,11 +11,30 @@ import { CartService } from "../_services/cart/cart.service";
 })
 export class CheckoutComponent implements OnInit {
   items: CartItem[] = [];
+  orderDetailsForm: FormGroup;
+  submitted = true;
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService, private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private checkoutService: Checkout
+  ) {
+    this.orderDetailsForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.pattern(/\+380[0-9]{9}/), Validators.required]],
+      address: ['', Validators.required],
+      deliveryTime: ['', Validators.required],
+      comment: [''],
+      disturb: [false]
+    })
+  }
 
   ngOnInit(): void {
     this.items = this.cartService.getCart().getItems();
+    console.log(this.authService.getMail());
+    
   }
 
   getSubtotalPrice(cartItem: CartItem): number {
@@ -29,6 +51,22 @@ export class CheckoutComponent implements OnInit {
 
   getPrice(cartItem: CartItem): number{
     return cartItem.goods.price-cartItem.goods.price*(cartItem.goods.discount/100);
+  }
+
+  isAuth(): boolean {
+    return this.authService.isAuthenticated();
+  }
+
+  get getForm(): { [p: string]: AbstractControl } { return this.orderDetailsForm.controls; }
+
+  onSubmit(): void {
+    if(this.orderDetailsForm.invalid) {
+      return;
+    }
+    
+
+    this.checkoutService.sendOrderDetails(this.orderDetailsForm.value, this.items);
+
   }
 
 
