fc87adb8de6fc271755f92e53b7f9641e7f68473
Yuliia Nortman <ynortman@gmail.com>
2021-06-15 14:15:38 +0300

    Fix profile info

--- marketplace-client/src/marketplace/src/app/system-accounts/system-accounts-routing-module.ts
+++ marketplace-client/src/marketplace/src/app/system-accounts/system-accounts-routing-module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { SystemAccountComponent } from './system-account/system-account.component';
 import { RegisterStuffComponent } from './register-stuff/register-stuff.component';
 import { AccountListComponent } from './account-list/account-list.component';
-import { ProfileComponent } from './profile/profile.component';
 import { UpdateInfoComponent } from './update-info/update-info.component';
 
 const routes: Routes = [
@@ -14,7 +13,6 @@ const routes: Routes = [
       { path: 'couriers', component: AccountListComponent },
       { path: 'managers', component: AccountListComponent },
       { path: 'register-stuff', component: RegisterStuffComponent },
-      { path: 'role/:role/profile/:id', component: ProfileComponent },
       { path: 'role/:role/update-info/:id', component: UpdateInfoComponent },
       {
         path: '',
